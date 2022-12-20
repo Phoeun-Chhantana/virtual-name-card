bbf8b9b69c2da991169dc3911ced12c98a0f3bac
Phoeun-Chhantana <42382202+Phoeun-Chhantana@users.noreply.github.com>
2022-12-20 18:03:03 +0700

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "808bb1507eb83698a8e2341d5220f231",
-"index.html": "773ac432354a1603ead9eacf78b3aed5",
-"/": "773ac432354a1603ead9eacf78b3aed5",
-"main.dart.js": "a018adea207382ac2cf30d736c3229f2",
+"index.html": "090dd1a592c07669642cdcbaf6cd6f02",
+"/": "090dd1a592c07669642cdcbaf6cd6f02",
+"main.dart.js": "19677f153c7b8eb31f0cd7c233b6ee27",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
